96b51f3953b307272c6a85a5a138003fb6d65370
ominislayer <omnslayer@gmail.com>
2025-12-21 14:55:02 -0300

    feat: Reativa todas as rotas após correção do Firebase

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import WhatsAppButton from "./components/WhatsAppButton";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-// import PortfolioLogos from "./pages/PortfolioLogos";
-// import PortfolioSites from "./pages/PortfolioSites";
-// import PortfolioCamisas from "./pages/PortfolioCamisas";
-// import Login from "./pages/Login";
-// import AdminDashboard from "./pages/AdminDashboard";
-// import ManageLogos from "./pages/admin/ManageLogos";
-// import PortfolioLogos from "./pages/PortfolioLogos";
-// import ManageSites from "./pages/admin/ManageSites";
-// import ManageCamisas from "./pages/admin/ManageCamisas";
-
+import PortfolioLogos from "./pages/PortfolioLogos";
+import PortfolioSites from "./pages/PortfolioSites";
+import PortfolioCamisas from "./pages/PortfolioCamisas";
+import Login from "./pages/Login";
+import AdminDashboard from "./pages/AdminDashboard";
+import ManageLogos from "./pages/admin/ManageLogos";
+import ManageSites from "./pages/admin/ManageSites";
+import ManageCamisas from "./pages/admin/ManageCamisas";
 
 const queryClient = new QueryClient();
 
@@ -28,14 +26,14 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* <Route path="/portfolio/logos" element={<PortfolioLogos />} /> */}
-          {/* <Route path="/portfolio/sites" element={<PortfolioSites />} /> */}
-          {/* <Route path="/portfolio/camisas" element={<PortfolioCamisas />} /> */}
-          {/* <Route path="/login" element={<Login />} /> */}
-          {/* <Route path="/admin" element={<AdminDashboard />} /> */}
-          {/* <Route path="/admin/logos" element={<ManageLogos />} /> */}
-          {/* <Route path="/admin/sites" element={<ManageSites />} /> */}
-          {/* <Route path="/admin/camisas" element={<ManageCamisas />} /> */}
+          <Route path="/portfolio/logos" element={<PortfolioLogos />} />
+          <Route path="/portfolio/sites" element={<PortfolioSites />} />
+          <Route path="/portfolio/camisas" element={<PortfolioCamisas />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/admin" element={<AdminDashboard />} />
+          <Route path="/admin/logos" element={<ManageLogos />} />
+          <Route path="/admin/sites" element={<ManageSites />} />
+          <Route path="/admin/camisas" element={<ManageCamisas />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
